be3904e3653befd89fefbec1a84364e9bb0a1a32
drdrf11 <134973816+drdrf11@users.noreply.github.com>
2023-07-24 18:56:23 +0000

    adding server functionality

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wiki_wiki
+from mylib.logic import phrase as wiki_phrase
 
-print(wiki())
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API.  Call /search or /wiki"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Search for page in wikipedia"""
+
+    result = search_wiki(value)
+    return {"results": result}
+
+
+@app.get("/wiki/{value}")
+async def wiki(value: str):
+    """Retrieve page from wikipedia"""
+
+    result = wiki_wiki(value)
+    return {"results": result}
+
+
+@app.get("/phrase/{value}")
+async def phrase(value: str):
+    """Retrieve page from wikipedia and return phrases"""
+
+    result = wiki_phrase(value)
+    return {"phrases": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
